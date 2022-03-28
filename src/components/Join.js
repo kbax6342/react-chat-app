d8a5cb815be74b7ca6d18396079f2a2db9ed4ace
Kevin Nelms Baxter II <k3vin.baxt3r@gmail.com>
2022-03-28 15:50:19 -0400

    Monday start project

---
+++ src/components/Join.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Join = () => {
+    const [name, setName] = useState('')
+    const [room, setRoom] = useState('')
+
+    return(
+        <h1>Join This is more text</h1>
+    )
+}
+export default Join
